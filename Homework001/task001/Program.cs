b60e2a620c531b6f5cc45412295390c366df6489
Djava <maska_uz@mail.ru>
2022-09-19 16:35:37 +0300

    HomeWork001_Lesson001

---
+++ Homework001/task001/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число :  ");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число :  ");
+int num_2 = Convert.ToInt32(Console.ReadLine());
+
+if (num_1 > num_2)  {
+    Console.WriteLine("Максимальное число :  " + num_1);
+    Console.WriteLine("Минимально число :  "  + num_2);
+}
+else    {
+    Console.WriteLine("Максимальное число :  " + num_2);
+    Console.WriteLine("Минимально число :  "  + num_1);
+}
---
+++ Homework001/task004/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число : ");
+int N = Convert.ToInt32(Console.ReadLine());
+int start_N = 1;
+
+while (start_N < N) {
+    if (start_N % 2 == 0)
+        Console.WriteLine(start_N);
+        start_N++; 
+}
