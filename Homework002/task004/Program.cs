cb3f7c34ea52ad31c759c15530da5b0a65b044d1
Djava <maska_uz@mail.ru>
2022-09-27 14:24:16 +0300

    upload hw002 task004

--- Homework002/task004/Program.cs
+++ Homework002/task004/Program.cs
@@ -4,6 +4,8 @@
 // 1 2 3 4 5 6 7-> 4
 // 10 20 30 40 50 60 70 -> 40
 
+// Первый вариант 
+
 int[] array = {1, 2, 3, 4, 5, 6, 7};
 int length = array.Length;
 int index = 0;
@@ -13,8 +15,20 @@ while (index < length)
     sum = array[index] + sum;
     index++;
 }
-int ariff = sum / length;
-Console.WriteLine($"среднее арифметическое число = {ariff}");
+int average = sum / length;
+Console.WriteLine($"среднее арифметическое число = {average}");
 
+// Второй и лучший Вариант
 
+// Console.WriteLine("Введите планируемое количество чисел:");
 
+// int count = int.Parse(Console.ReadLine());
+// int sum = 0;
+// for (int i = 1; i <= count; i++)
+// {
+// Console.WriteLine($"Введите {i}-ое число:");
+// int number = int.Parse(Console.ReadLine());
+// sum = sum + number;
+// }
+// int average = sum / count;
+// Console.WriteLine(average);
