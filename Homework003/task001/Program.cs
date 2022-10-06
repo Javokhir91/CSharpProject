582be67f6855a65be20abf5f613ded2c9ee99acc
Djava <maska_uz@mail.ru>
2022-10-07 01:32:57 +0300

    homework003

---
+++ Homework003/task001/Program.cs
@@ -0,0 +1,43 @@
+﻿#region 
+/*Задача №19
+Напишите метод, который принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+#endregion
+
+Console.Clear(); // для очистки консол
+
+#region ReadData //  читаю данные от пользователя
+int ReadData(string line) //  читаю данные от пользователя
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+#endregion
+
+#region PrintData // вывожу данные
+void PrintData(string text, string value) // вывожу данные
+{
+    Console.WriteLine(text + value);
+}
+#endregion
+
+#region PaliTest // проверяю число на палиндром..
+string PaliTest(int number) // проверяю число на палиндром..
+{
+    int pal = 0;
+    int num = number;
+    while (number > 0)
+    {
+        pal = pal * 10 + number % 10; number = number / 10;
+    }
+    return num == pal ? "Палиндром" : "Не палиндром";
+}
+#endregion
+
+int user = ReadData("Введите число: ");
+PrintData("Число: ", PaliTest(user));
---
+++ Homework003/task003/Program.cs
@@ -0,0 +1,47 @@
+﻿#region Задача №23
+/*Задача №23
+напишите программу, которая принимает на вход число (N) и 
+выдает таблицу кубов чисел от 1 до N.
+Например:
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+#endregion
+
+Console.Clear(); // для очистки консол
+
+#region ReadData // читаю данные от пользователя
+int ReadData(string line) //  читаю данные от пользователя
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+#endregion
+
+#region PrintData // вывожу данные
+void PrintData(string text, string value) // вывожу данные
+{
+    Console.WriteLine(text + value);
+}
+#endregion
+
+#region LineNum // нахожу длину и возведение
+string LineNum(int numN, int p) // нахожу длину и возведение 
+{
+    string outLine = "1";
+    for (int i = 2; i <= numN; i++)
+    {
+        outLine = outLine + " " + Math.Pow(i, p);
+    }
+    return outLine;
+}
+#endregion
+
+int numN = ReadData("Введите число N: ");
+
+string lineTop = LineNum(numN, 1);
+string lineDown = LineNum(numN, 3);
+
+PrintData("", lineTop);
+PrintData("", lineDown);
